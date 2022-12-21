6d1185d94aa9ae72865db900ee5383608dbdb9f3
Rendra Saputra <rendraseptian24@gmail.com>
2022-12-22 00:28:49 +0700

    update wilayah api

--- app/Http/Controllers/Api/ResourceApiController.php
+++ app/Http/Controllers/Api/ResourceApiController.php
@@ -9,15 +9,21 @@ use App\Http\Controllers\Controller;
 
 class ResourceApiController extends Controller
 {
-    public function wilayah()
+    public function wilayah(Request $request)
     {
         $id = 35;
         $n=strlen($id);
         $m=($n==2?5:($n==5?8:13));
         $wilayah = Wilayah::whereRaw("LEFT(kode, {$n})='{$id}'")
-                       ->whereRaw("CHAR_LENGTH(kode)={$m}")
-                    ->orderBy('nama', 'ASC')->get();
-                    return response()->json(['status' => true, 'message' => 'Berhasil mendapatkan data wilayah!', 'data' => $wilayah]);
+                       ->whereRaw("CHAR_LENGTH(kode)={$m}");
+                    
+        if($request->searchTerm){
+            $wilayah->where('nama', 'like', '%'. $request->searchTerm .'%');
+        }
+
+        $return_data = $wilayah->orderBy('nama', 'ASC')->get();
+
+                    return response()->json(['status' => true, 'message' => 'Berhasil mendapatkan data wilayah!', 'data' => $return_data]);
     }
 
     public function profesi()
